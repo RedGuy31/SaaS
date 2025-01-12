89d6b291423262516e0a64e071598b72bca28a4d
Irakli_Maisuradze(Redguy31) <irakli.maisuradze31.19@gmail.com>
2025-01-12 20:18:09 +0400

    first commit

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/utils";
+import { ReactNode } from "react";
+
+interface MaxWdthWrapperProps {
+  className?: string;
+  children: ReactNode;
+}
+
+export const MaxWidthWrapper = ({
+  className,
+  children,
+}: MaxWdthWrapperProps) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
